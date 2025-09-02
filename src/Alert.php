015d87b68b09c14b0c8e298de03ce0b848a25783
Italo Israel Baeza Cabrera <DarkGhostHunter@Gmail.com>
2025-09-01 22:59:07 -0400

    Should fix setting values into the Fluent instance.

--- src/Alert.php
+++ src/Alert.php
@@ -330,10 +330,12 @@ class Alert implements Arrayable, Jsonable, JsonSerializable, Stringable
      */
     public function metadata(array|string $key, mixed $value = null): static
     {
-        if (is_array($key)) {
-            $this->metadata->fill($key);
-        } else {
-            $this->metadata->set($key, $value);
+        if (is_string($key)) {
+            $key = [$key => $value];
+        }
+
+        foreach ($key as $name => $value) {
+            $this->metadata[$name] = $value;
         }
 
         return $this;
